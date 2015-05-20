e6bcd8d2a716c83ed3f3550894bd575de63ad5e4
Leandro Ostera <leandro@ostera.io>
2015-05-20 16:12:50 -0300

    Adds FSM-to-FSM API

--- src/trade_fsm.erl
+++ src/trade_fsm.erl
@@ -54,3 +54,50 @@ ready(OwnPid) ->
 cancel(OwnPid) ->
   gen_fsm:sync_send_all_state_event(OwnPid, cancel).
 
+%%%
+%%% FSM to FSM API
+%%%
+
+%% Ask the other FSM for a trade session
+ask_negotiate(OtherPid, OwnPid) ->
+  gen_fsm:send_event(OtherPid, {ask_negotiate, OwnPid}).
+
+%% Let the other paty know we're doing it
+accept_negotiate(OtherPid, OwnPid) ->
+  gem_fsm:send_event(OtherPid, {accept_negotiate, OwnPid}).
+
+%% Sends an offer
+do_offer(OtherPid, Item) ->
+  gem_fsm:send_event(OtherPid, {do_offer, Item}).
+
+%% Un-send offer
+undo_offer(OtherPid, Item) ->
+  gen_fsm:send_event(OtherPid, {undo_offer, Item}).
+
+%% Asks the other side if it's ready
+are_you_ready(OtherPid) ->
+  gen_fsm:send_event(OtherPid, are_you_ready).
+
+%% We're not ready yet - reply.
+not_yet(OtherPid) ->
+  gen_fsm:send_event(OtherPid, not_yet).
+
+%% we're ready and willing and waiting for a ready state.
+am_ready(OtherPid) ->
+  gen_fsm:send_event(OtherPid, 'ready!').
+
+%% acknowledges that we're in the ready state
+ack_trans(OtherPid) ->
+  gen_fsm:send_event(OtherPid, ack).
+
+%% Asks if it's ready to commit
+ask_commit(OtherPid) ->
+  gen_fsm:sync_send_event(OtherPid, ask_commit).
+
+%% Begins a synchronous commit
+do_commit(OtherPid) ->
+  gen_fsm:sync_send_event(OtherPid, do_commit).
+
+%% Courteously notify we're cancelling
+notify_cancel(OtherPid) ->
+  gen_fsm:send_all_state_event(OtherPid, cancel).
